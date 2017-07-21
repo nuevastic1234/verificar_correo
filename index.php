077490908db66e80fc96ab7de7067c9cf15a1029
Juan Diego Ninco Collazos <juandiego42011@hotmail.com>
2017-07-20 22:56:29 -0500

    se modifico el index

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
 </head>
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <body>
-<?php 
-	include('conexion.php');
- ?>
+<?php
+include 'conexion.php';
+?>
 	 <div align="center">
 	 <h3>IMPORTAR ARCHIVO CON EXTENSIÓN CSV Y VERIFICAR CORREOS</h3>
+
+
 		  <h3>Estructura</h3>
 	 	  <img src="estructura.png">
 		  <br>
@@ -20,7 +22,7 @@
 		  </form>
 	<br>
 	<input type="button" onclick="location='generar_excel.php'" id="excel"  value="GENERAR EXCEL CORREOS VERIFICADOS" style="display:none" />
-	
+
 	<input type="button" onclick="location='eliminar_todo.php'" id="elimina"  value="ELIMINAR TODOS LOS REGISTROS" style="display:none" />
 
 	<input type="button" onclick="location='verificacion.php'" id="verifica"  value="VERIFICAR CORREOS Y DOMINIOS" style="display:none" />
@@ -28,45 +30,42 @@
 	<br>
 	<hr>
 
-	 <?php 
-	 		  if (count($min)>0 && count($max)>0) {
-	 		  		$datos = $conn->query('SELECT * FROM usuario where id between '.$min.' and '.$max);
-	 		  }
+	 <?php
+if (count($min) > 0 && count($max) > 0) {
+    $datos = $conn->query('SELECT * FROM usuario where id between ' . $min . ' and ' . $max);
+}
 
-			  if (!isset($datos)) 
-			  {
-			  	echo "<div style='width:500px;height:30px;background:#FFB5B5'>No hay registros para listar</div>";
-			  }
-			  else
-			  {
-			  	echo "<script>$('#excel').fadeIn(1500); $('#elimina').fadeIn(1500);</script>";
-			  	echo "<script>$('#excel').fadeIn(1500); $('#verifica').fadeIn(1500);</script>";
+if (!isset($datos)) {
+    echo "<div style='width:500px;height:30px;background:#FFB5B5'>No hay registros para listar</div>";
+} else {
+    echo "<script>$('#excel').fadeIn(1500); $('#elimina').fadeIn(1500);</script>";
+    echo "<script>$('#excel').fadeIn(1500); $('#verifica').fadeIn(1500);</script>";
 
-				 $datos = $conn->query('SELECT * FROM usuario where id between '.$min.' and '.$max);
+    $datos = $conn->query('SELECT * FROM usuario where id between ' . $min . ' and ' . $max);
 
-			  	echo "<table border='2'>".
-			  	"<thead>".
-			  	"<tr>".
-			  	"<th>Num</th>".
-				"<th>Correo</th>".
-				"<th>Dominio</th>".
-				"<th>Valido</th>".
-				"</tr>".
-			  	"</thead>";
-			  	foreach ($datos as $row) {
-			  		echo 
-			  		"<tbody>".
-					"<tr>".
-			  		"<td>".$row['num']."</td>".
-			  		"<td>".$row['correo']."</td>".
-			  		"<td>".$row['tipo']."</td>".
-			  		"<td>".$row['valido']."</td>".
-			  		"</tr>".
-			  		"</tbody>";
-			  	}
-			  	echo "</table>";
-			  }
-	  ?>
+    echo "<table border='2'>" .
+        "<thead>" .
+        "<tr>" .
+        "<th>Num</th>" .
+        "<th>Correo</th>" .
+        "<th>Dominio</th>" .
+        "<th>Valido</th>" .
+        "</tr>" .
+        "</thead>";
+    foreach ($datos as $row) {
+        echo
+            "<tbody>" .
+            "<tr>" .
+            "<td>" . $row['num'] . "</td>" .
+            "<td>" . $row['correo'] . "</td>" .
+            "<td>" . $row['tipo'] . "</td>" .
+            "<td>" . $row['valido'] . "</td>" .
+            "</tr>" .
+            "</tbody>";
+    }
+    echo "</table>";
+}
+?>
 
 	 </div>
 
